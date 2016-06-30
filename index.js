76174c824d9e62471f766ee8e5b74becbf55f86f
孙圣达 <sunshengda@meituan.com>
2016-06-30 10:48:56 +0800

    wx

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var koa = require("koa")
 var app = koa()
 var Router = require('trie-koa-router')
 var router = new Router()
+var _static = require('koa-static');
+app.use(_static('./assets'));
+var handlebars = require("koa-handlebars");
+app.use(handlebars({
+    viewsDir: "views"
+}));
 app.use(router.dispatcher())
 
 router.route('/').get(function* (next) {
-  this.body = '振靖 你丫现在辞职 我还佩服你'
-})
+    yield this.render("index", {});
+});
 
 app.listen(8080)
