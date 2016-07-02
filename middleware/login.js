e5757b92c53862859a6108cd4f9126d6931807eb
SunPeter <ssd3237649@gmail.com>
2016-07-02 15:47:09 +0800

    update

--- middleware/login.js
+++ middleware/login.js
@@ -1,19 +1,26 @@
 var getToken = require("../service/getToken");
 var getTicket = require("../service/getTicket");
+var checkTicket = require("../service/checkTicket");
 module.exports = function(app) {
     return function*(next) {
-        app.ticket = "kgt8ON7yVITDhtdwci0qeYvFergJmeNl2u8ZkGCb8BHxzyO0ZOxB72_faGWd-OMWO8PPtBJaZ_FJAmtsjMVrEg";
-        if (!app.ticket) {
-            var res = yield getToken();
-            var info = JSON.parse(res.body);
-            app.access_token = info.access_token;
-            console.log(app.access_token);
-
-            res = yield getTicket(app.access_token);
-            info = JSON.parse(res.body);
-            app.ticket = info.ticket;
-            console.log(app.ticket)
+        app.ticket = "kgt8ON7yVITDhtdwci0qeYvFergJmeNl2u8ZkGCb8BEZRNM6PmeyO3riIUxofnH2OsD9hPebUQfe87QT5Dr40A";
+        if (app.ticket && app.ticket_expires && ( (Date.now() - app.ticket_expires) / 1000 ) > 7000 || !app.ticket) {
+            yield createTicket(app);
         }
         yield * next;
     }
+}
+
+function* createTicket(app) {
+    var res = yield getToken();
+    var info = JSON.parse(res.body);
+    app.access_token = info.access_token;
+    console.log("token:", app.access_token);
+
+    res = yield getTicket(app.access_token);
+    info = JSON.parse(res.body);
+    app.ticket = info.ticket;
+    console.log("ticket:", app.ticket)
+
+    app.ticket_expires = Date.now();
 }
