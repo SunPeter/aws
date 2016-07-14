a2af097bc9853e0ba97d1a18778d4e15eb2af923
孙圣达 <sunshengda@meituan.com>
2016-07-14 08:34:16 +0800

    merge mobile

---
+++ assets/js/index_mobile.js
@@ -0,0 +1,127 @@
+;$(function() {
+	var wrap = $("#wrap");
+	window.onhashchange = function () {
+		var hash = location.hash.match(/#!(.+)$/);
+		var menu = hash && hash[1] || "";
+		wrap.removeClass();
+		switch (menu) {
+			case "business":
+				wrap.addClass("three");
+				break;
+			case "join":
+				wrap.addClass("two");
+				break;
+			case "contact":
+				wrap.addClass("one");
+				break;
+			default:
+				return;
+		}
+	}
+
+	var list = [{
+		content: '<div class="slider-item"><p class="p1">来这里，享受最专业的</p><p class="p1" style="margin-bottom: .10rem;">创意视频&nbsp;制作服务</p><p>“我们可能是世界上最糟糕的广告公司，</p><p>《<span class="yellow">世糟广</span>》因而成为了公司名字”</p></div>'
+	}, {
+		content: '<div class="slider-item"><p class="p1">来这里，享受最专业的</p><p class="p1" style="margin-bottom: .10rem;">创意视频&nbsp;制作服务</p><p>“我们可能是世界上最糟糕的广告公司，</p><p>《<span class="yellow">世糟广</span>》因而成为了公司名字”</p></div>'
+	}]
+
+	var S = new iSlider(document.getElementById("slider"), list, {
+        isAutoplay: 1,
+        isLooping: 1,
+        isOverspread: 1,
+        animateTime: 800,
+		plugins: ['dot']
+    });
+
+	var nav = $("#nav_wrap");
+	nav.on("click", function(e){
+		var parent = $(this).parent();
+		e.stopPropagation()
+		e.preventDefault();
+		if (parent.hasClass("active")) {
+			parent.removeClass("active");
+		} else {
+			parent.addClass("active");
+		}
+	})
+	$(".nav-menu a").on("click", function () {
+		$("nav").removeClass("active");
+	})
+
+	$("#join-btn").click(function () {
+		var name = $("#joinus .j-name"), phone = $("#joinus .j-phone"), remark = $("#joinus .j-remark");
+		if (!name || !phone || !remark) {
+			alert("您填写的信息不完整");
+			return;
+		}
+		var data = {
+			name: name.val(),
+			contact_type: phone.val(),
+			remark: remark.val()
+		}
+		$.post("http://ideaology.cn/joinus.html", data, function (res) {
+			alert(res.msg);
+	        if(200==res.status){
+	            location.reload();
+	        }
+		})
+	})
+	var step3 = $("#step3");
+	$(".tab li").on("click", function () {
+		var target = $(this);
+		if (target.index() === 0) {
+			step3.removeClass("next");
+		} else {
+			step3.addClass("next");
+		}
+	});
+	$("#consume-btn").click(function () {
+		var name = $("#consume .j-name"), email = $("#consume .j-email"), describe = $("#consume .j-describe");
+		if (!name || !email || !describe) {
+			alert("您填写的信息不完整");
+			return;
+		}
+		var data = {
+			name: name.val(),
+			business_info: describe.val(),
+			email: email.val(),
+			self_desc: "",
+			feedback1: "",
+			feedback2: "",
+			project_end_time: "",
+			project_budget: "",
+			other: ""
+		}
+		$.post("http://ideaology.cn/contactus.html", data, function (res) {
+			alert(res.msg);
+	        if(200==res.status){
+	            location.reload();
+	        }
+		})
+	})
+	$("#business-btn").click(function () {
+		var name = $("#business .j-name"), email = $("#business .j-email"), describe = $("#business .j-describe"), remark = $("#business .j-remark");
+		if (!name || !email || !describe) {
+			alert("您填写的信息不完整");
+			return;
+		}
+		var data = {
+			name: name.val(),
+			remark: remark.val(),
+			business_info: describe.val(),
+			email: email.val(),
+			self_desc: "",
+			feedback1: "",
+			feedback2: "",
+			project_end_time: "",
+			project_budget: "",
+			other: ""
+		}
+		$.post("http://ideaology.cn/contactus.html", data, function (res) {
+			alert(res.msg);
+	        if(200==res.status){
+	            location.reload();
+	        }
+		})
+	})
+})
--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,52 @@
 'use strict';
 
 var path = require('path'),
-    gulp = require('gulp'),
-    compass = require('gulp-compass'),
-    browserSync=require("browser-sync"),
-    noop = function () {};
+	gulp = require('gulp'),
+	compass = require('gulp-compass'),
+	browserSync = require("browser-sync"),
+	spawn = require('child_process').spawn,
+	node,
+	noop = function() {};
 
-gulp.task('compass', function () {
-    gulp.src('./assets/sass/*.scss')
-        .pipe(compass({
-            config_file: './assets/config.rb',
-            css: './assets/css',
-            sass: './assets/sass'
-        }));
-});
-
-gulp.task("browser-sync",function(){
-	var files=[
-	"./assets/css/*.css",
-	"./assets/js/*.js"
-	]
-    browserSync.init(files, {
-       proxy: "localhost:8080"
-   });
-});
-
-gulp.task("watch",function(){
-	gulp.watch("./assets/sass/*",["compass"])
+gulp.task('server', function() {
+	if (node) node.kill()
+	node = spawn('node', ['index.js'], {
+		stdio: 'inherit'
+	}, function (err, res) {
+	    console.log(err || res);
+	})
+	node.on('close', function(code) {
+		if (code === 8) {
+			gulp.log('Error detected, waiting for changes...');
+		}
+	});
 })
-gulp.task('default', ['watch', 'browser-sync'], noop);
+
+gulp.task('compass', function() {
+	gulp.src('./assets/sass/*.scss')
+		.pipe(compass({
+			config_file: './assets/config.rb',
+			css: './assets/css',
+			sass: './assets/sass'
+		}))
+		.on('error', function(error) {
+	      console.error(error);
+		  if (node) node.kill();
+	    });
+});
+
+gulp.task("browser-sync", function() {
+	var files = [
+		"./assets/css/*.css",
+		"./assets/js/*.js"
+	]
+	browserSync.init(files, {
+		proxy: "localhost:8080"
+	});
+});
+
+gulp.task("watch", function() {
+	gulp.watch("./assets/sass/*", ["compass"])
+    gulp.watch("./views/*", ["server"])
+})
+gulp.task('default', ['server', 'watch', 'browser-sync'], noop);
